9b8d8b46d9b9265e2db63103f1d427db02fe3521
D-Lawson <dan.lawson@live.co.uk>
2021-08-03 00:19:52 +0000

    Add learner name function to render text input fields, update CSS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Get and format the date
+
 var dateValue = document.getElementById('date-value');
 
 var todayDate = new Date(Date.now());
@@ -24,4 +26,39 @@ $(".set-today").on("click", function () {
 
 $(".set-yesterday").on("click", function () {
     $('#date-value').html(yesterdayDate);
+});
+
+// Learner name function
+
+function addFields1() {
+    // Get number value
+    var number = document.getElementById("number-field").value;
+    // Clear contents
+    $("#table-style .remove").parents("tr").remove();
+    // Add table rows to enter learner names
+    for (i = 0; i < number; i++) {
+
+        var table = document.getElementById("table-style");
+        var row = table.insertRow(8 + i);
+        row.className = 'cell-1';
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+
+        // Inner HTML for learner name
+        cell1.innerHTML = `<label class="form-labels remove" for="learner-name-${[i+1]}">Learner name ${[i+1]}:</label>`;
+        cell2.innerHTML = `<input type="text" class="form-control remove" id="learner-name-${[i+1]}" name="learner-name-${[i+1]}"
+        placeholder="Enter name" required>`;
+    }
+}
+
+// Document ready with jquery, call learner name function
+
+$(document).ready(function () {
+
+    $("#number-field").change(function () {
+        $.fn.myFunction();
+    });
+    $.fn.myFunction = function () {
+        addFields1();
+    };
 });
